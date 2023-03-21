67e9dfee33a09950f09588c1d6f354d9ccd967db
Jose Viosca Ros <jviosca@gmail.com>
2023-03-21 12:06:14 +0100

    Add approach png

--- app.py
+++ app.py
@@ -35,19 +35,15 @@ st.title(':blue[People in STEM in news reporting in Spanish written press]')
 tab1, tab2 = st.tabs(['Gender Bias', 'Methodology'])
 with tab1:
     show_image('img/distribution_article_mentions_genre.png') 
-    st.write('This webapp showcases a **recommender system** built from records \
-            of user interactions with content items at \
-            [IBM Watson Studio](https://www.ibm.com/cloud/watson-studio).')      
+    st.write('')      
     show_image('img/distribution_people_mentions_genre.png')
-    st.write('The list of recommendations is **updated dynamically** according \
-             to user activity. To interact with the engine and see its results, \
-             click on the *Recommender* tab above and simulate user activity on the \
-             platform by picking articles to read. You can obtain \
-             personalized recommendations for you or other users!')
+    st.write('')
     show_image('img/unique_mentions_genre_stats.png')
+    st.write('')
     show_image('img/people_per_article_genre.png')
 
 
 with tab2:
+    show_image('img/Approach.png')
     st.write('For more information, visit the [GitHub repository](https://github.com/jviosca/Media-portrayal-of-People-in-STEM-in-Spanish-written-press).')  
     st.write('Author: [Jose Viosca Ros](https://jvros.com.es/index.php/es/inicio/)')           
